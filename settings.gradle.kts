6910d44db77f4212130218915b537283bd10916a
NeptunoH <122707858+NeptunoH@users.noreply.github.com>
2024-03-24 23:39:38 -0600

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,19 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+
+rootProject.name = "PM1Tarea4Parcial2"
+include(":app")
+ 
